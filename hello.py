7e9476f093b41b392eddda49129bc9c122d909de
VaibhavRajpoot <vaibhavrajpoot46@gmail.com>
2023-08-26 22:15:55 +0530

    will commit

---
+++ hello.py
@@ -0,0 +1,65 @@
+# #a= "Harry"
+# #print( a[-2:-1])
+
+
+# # a=[2,3,5,7,1]
+# # print (a.index(2) )
+
+
+# # quess 1
+# # f1= input ("Input the fruit name ")
+# # f2= input ("Input the fruit name ")
+# # f3= input ("Input the fruit name ")
+# # f4= input ("Input the fruit name ")
+# # f5= input ("Input the fruit name ")
+# # f6= input ("Input the fruit name ")
+# # f7= input ("Input the fruit name ")
+
+# # fruits=[f1,f2,f3,f4,f5,f6,f7]
+# # print (fruits)
+
+# # dict= {"name":"Harry","from":"India","marks":[92,98,96]}
+# # dict.update({"friend":"Sam"})
+# # print(dict.items())
+
+# # hindidict={"book":"kitaab","you":"tum","me":"mein"}
+# # a= input("type a word to find its hindi meaning")
+# # print ("hindi meaning of this word is ",hindidict.get(a))
+
+
+# # number1= int(input ("Input a number "))
+# # number2= int(input ("Input a number "))
+# # number3= int(input ("Input a number "))
+# # number4= int(input ("Input a number "))
+# # number5= int(input ("Input a number "))
+# # number6= int(input ("Input a number "))
+# # number7= int(input ("Input a number "))
+# # number8= int(input ("Input a number "))
+
+# # a=set()
+# # a.add(number1)
+# # a.add(number2)
+# # a.add(number3)
+# # a.add(number4)
+# # a.add(number5)
+# # a.add(number6)
+# # a.add(number7)
+# # a.add(number8)
+
+
+# # print(a)
+
+
+# a= {'setting','betting',('a','b','dd')}
+# print (a)
+
+# b= (4,7,4,3,[78,45])
+# print(b)
+
+# c=[56,45,33,{'vaibhav','niki','raju'}]
+# print(c)
+
+
+a=(72,33,11,65,23,210,['akash','abhi'])
+a=a[4]
+print(a)
---
+++ lessthan10char.py
@@ -0,0 +1,6 @@
+username= input("input the username of the person ")
+a= len(username)
+if(a>10):
+    print("total alphabets are more than 10")
+else:
+    print( "total alphabets are less than 10")
