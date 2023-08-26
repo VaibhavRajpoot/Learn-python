4bc495101e0364ab9b7b0ea3bdc6bcba57da9bb1
VaibhavRajpoot <vaibhavrajpoot46@gmail.com>
2023-08-26 22:15:17 +0530

    commit done

---
+++ greatestoffour.py
@@ -0,0 +1,10 @@
+a= int(input("Input the number: "))
+b= int(input("Input second number:"))
+c= int(input("Input the third number: "))
+d= int(input("Input the fourth number: "))
+if(a>b and a>c):
+    if(a>d):
+        print("greatest of all is :",a)
+    else:
+        print("greatest of all is :",d)
+
